f1a4b1e72b039aed6ebc79fb6886eac2f8c8215e
Luiz Fernando de Frias <l.frias@globo.com>
2010-11-25 14:16:42 -0200

    Consertada o preenchimento automático da Data

--- media/js/actions.js
+++ media/js/actions.js
@@ -6,6 +6,45 @@
  */
 
 /*-------------------------- Global Functions ------------------------------*/
+(function($){
+	$.fn.writePortugueseDate = function(){
+		var element = $(this[0]);
+		var mydate=new Date()
+		var year=mydate.getYear()
+		if (year<2000)
+		year += (year < 1900) ? 1900 : 0
+		var day=mydate.getDay()
+		var month=mydate.getMonth()
+		var daym=mydate.getDate()
+		if (daym<10)
+		daym="0"+daym
+		var dayarray=new Array(
+			"Domingo",
+			"Segunda-feira",
+			"Terça-feira",
+			"Quarta-feira",
+			"Quinta-feira",
+			"Sexta-feira",
+			"Sábado"
+		);
+		var montharray=new Array(
+			"de Janeiro de ",
+			"de Fevereiro de ",
+			"de Março de ",
+			"de Abril de ",
+			"de Maio de ",
+			"de Junho de",
+			"de Julho de ",
+			"de Agosto de ",
+			"de Setembro de ",
+			"de Outubro de ",
+			"de Novembro de ",
+			"de Dezembro de "
+		);
+		var msg = dayarray[day]+", "+daym+" "+montharray[month]+year;
+		element.val(msg);
+	};
+})(jQuery);
 
 function writeTable(xmlstring, div){
 	var booleanColor = true;
@@ -58,6 +97,8 @@ function getScrollXY() {
 
 //Document is ready, let's play
 $(document).ready(function(){
+
+	$('#dataPreenchimento').writePortugueseDate();
 
 /*------------------------------Edition and Relation-----------------------------*/
 	//Relation between forms
